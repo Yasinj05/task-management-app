c0bd7766a4a1bf49af47309ded532fbaf37262e1
Yasin Jafari <yasinj7658@gmail.com>
2024-05-14 17:14:31 +0330

    Update main.ts and database configuration in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,25 +2,29 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { TaskModule } from './task/task.module';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
+import { ProfileModule } from './profiles/profile.module';
+import { TasksModule } from './tasks/tasks.module';
+import { User } from './users/entities/user.entity';
+import { Task } from './tasks/entities/task.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT, 10) || 3306,
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'Aa@123456',
-      database: process.env.DB_NAME || 'task_management_db',
-      entities: ['dist/**/*.entity{.ts,.js}'],
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Task],
       synchronize: true,
     }),
     AuthModule,
-    TaskModule,
-    UserModule,
+    UsersModule,
+    ProfileModule,
+    TasksModule,
   ],
 })
 export class AppModule {}
